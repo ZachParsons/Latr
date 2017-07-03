d7b8f07db61b5ad1130d5edd093c72cd7a36fe18
jackhowa <johncarverhoward@gmail.com>
2017-07-03 15:23:32 -0500

    add sign in and password and sign in buttons' ib outlets

--- SnapClone/SignInViewController.swift
+++ SnapClone/SignInViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SignInViewController.swift
 //  SnapClone
 //
 //  Created by Jack Howard on 7/3/17.
@@ -8,8 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SignInViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var signInButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
