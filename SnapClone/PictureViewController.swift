421e42a9dfb48db6b0e5d0c03cc7516b8779d6e4
jackhowa <johncarverhoward@gmail.com>
2017-07-03 19:46:58 -0500

    add image view and ui photos view with back and forward camera

---
+++ SnapClone/PictureViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PictureViewController.swift
+//  SnapClone
+//
+//  Created by Jack Howard on 7/3/17.
+//  Copyright © 2017 JackHowa. All rights reserved.
+//
+
+import UIKit
+
+class PictureViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var nextButton: UIButton!
+    
+    @IBOutlet weak var descriptionTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func tappedCamera(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func tappedNext(_ sender: Any) {
+        
+    }
+
+}
