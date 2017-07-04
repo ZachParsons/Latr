88723a92967ae3d2513c4fc3daa6c82a7215a044
jackhowa <johncarverhoward@gmail.com>
2017-07-04 08:57:15 -0500

    add table view for list of user recipients

--- SnapClone/SelectUserViewController.swift
+++ SnapClone/SelectUserViewController.swift
@@ -8,28 +8,27 @@
 
 import UIKit
 
-class SelectUserViewController: UIViewController {
+class SelectUserViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // using delegate to reference the table view outlet
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // add firebase users 
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
     }
-    */
-
 }
