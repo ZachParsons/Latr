203104ea239b6c770c6d04c7313272fc5e1b7687
jackhowa <johncarverhoward@gmail.com>
2017-07-04 10:17:30 -0500

    show user recipients from firebase storage data that were recently appended

--- SnapClone/SelectUserViewController.swift
+++ SnapClone/SelectUserViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class SelectUserViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
@@ -21,6 +22,36 @@ class SelectUserViewController: UIViewController, UITableViewDataSource, UITable
         self.tableView.dataSource = self
         self.tableView.delegate = self
         
+        // call the database of firebase
+        // listen for db changes at a particular place or index
+        // tell us about any childadded
+        // want to know all of the new users that are added
+        
+        // child users is the header in the storage
+        Database.database().reference().child("users").observe(DataEventType.childAdded, with: {(snapshot) in
+            // returns object of each user 
+            // called for each user
+            print(snapshot)
+            
+            // like calling new
+            let user = User()
+            
+            // setting the values
+            // forcing the value as well as the upcast to a string
+            
+            // need to cast snapshot.value as a NSDictionary.
+            let value = snapshot.value as? NSDictionary
+            
+            user.email = value?["email"] as! String
+            
+            // snapshot dictionary doesn't have a key so can keep this
+            user.uid = snapshot.key // assigns the uid
+            
+            // kind of like shovelling back into users
+            self.users.append(user)
+            
+            self.tableView.reloadData()
+        })
     }
 
     // add firebase users 
@@ -37,7 +68,7 @@ class SelectUserViewController: UIViewController, UITableViewDataSource, UITable
         // calling the cell at the index
         let user = users[indexPath.row]
         
-        // assign the label of text 
+        // assign the label of text
         cell.textLabel?.text = user.email
         
         return cell
