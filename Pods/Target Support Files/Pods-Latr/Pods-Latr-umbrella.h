b02e4f9890d280c764dc97da522b2efabd6f6543
bmerkle3 <braidymerkle@gmail.com>
2017-07-08 17:01:37 -0500

    add code for post req and date parser

--- Pods/Target Support Files/Pods-Latr/Pods-Latr-umbrella.h
+++ Pods/Target Support Files/Pods-Latr/Pods-Latr-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_SnapCloneVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_SnapCloneVersionString[];
+FOUNDATION_EXPORT double Pods_LatrVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_LatrVersionString[];
 
